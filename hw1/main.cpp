e7c7a85e291b291e14974820f74b5f5b9e47758c
tend69 <90572796+tend69@users.noreply.github.com>
2021-12-21 18:19:08 +0300

    v1

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
 
+
+int main(){
+    std::ifstream input_file("in.txt");
+    int a
+    input_file>>a;
+}
+cin>>xn>>yn
+a=atan(xn/yn)
+for  (i=1; i<10; i++)
+    {dl=0
+    cin>>x>>y
+    x1=x*cos(a)+y*sin(a)
+    y1=y*cos(a)-x*sin(a)
+
+
+    if (x1^2+y1^2>dl)
+        {dl=x1^2(i)+y1^2(i)
+        n=i}
+    }
