ca4cc2f1871a915fd31d0f745c504b8f7815e79c
tend69 <90572796+tend69@users.noreply.github.com>
2021-12-22 16:40:12 +0300

    v3

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <valarray>
 
 
 int main(){
     std::ifstream input_file("in.txt");
-    int a
-    input_file>>a;
+    float xn;
+    float yn;
+    input_file >> xn >> yn;
+    float xr=xn;
+    float xl=xn;
+    float yr=yn;
+    float yl=yn;
+    double a = atan(xn/yn);
+    while (input_file){
+        input_file >> xn >> yn;
+        float x1=xn*cos(a)+yn*sin(a);
+        float y1=yn*cos(a)-xn*sin(a);
+        if (x1>xr){
+            x1=xr;
+            y1=yr;
+        }
+        if (x1<xl){
+            x1=xl;
+            y1=yl;
+        }
+    }
+    cout << "Самая правая: " << xr << ' ' << yr;
+    cout << "Самая левая:" << xl << ' ' << yl;
+    return 0;
 }
-cin>>xn>>yn
-        a=atan(xn/yn)
-for  (i=1; i<10; i++)
-{dl=0
-cin>>x>>y
-        x1=x*cos(a)+y*sin(a)
-y1=y*cos(a)-x*sin(a)
-
-
-if (x1^2+y1^2>dl)
-{dl=x1^2(i)+y1^2(i)
-n=i}
-}
-
